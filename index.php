9ddde9fbc5f282f88ac0e28f564bb80407af3ee1
herie89 <93757039+herie89@users.noreply.github.com>
2021-12-14 23:18:28 +0700

    template_latihan
    Template untuk latihan

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Home Admin</title>
+</head>
+<body>
+
+    <input type="checkbox" name="" id="menu-toggle">
+
+    <div class="sidebar">
+        <div class="sidebar-container">
+            <div class="brand">
+                <h2>
+                    <span class="lab la-staylinked"></span>
+                    Ecommerce
+                </h2>
+            </div>
+
+            <div class="sidebar-avartar">
+                <div>
+                    <img src="img/profile.png" alt="">
+                </div>
+                <div class="avartar-info">
+                    <div class="avartar-text">
+                        <h4>Kaziya Nisrina</h4>
+                        <small>14-12-2021</small>
+                    </div>
+                    <span class="las la-angle-double-down"></span>
+                </div>
+            </div>
+
+            <div class="sidebar-menu">
+                <ul>
+                    <li>
+                        <a href="" class="active">
+                            <span class="las la-adjust"></span>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="las la-video"></span>
+                            <span>Videos</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="las la-chart-bar"></span>
+                            <span>Analytic</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="las la-calendar"></span>
+                            <span>Schedule</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span class="las la-user"></span>
+                            <span>Acount</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="sidebar-card">
+                <div class="side-card-icon">
+                    <span class="lab la-codiepie"></span>
+                </div>
+                <div>
+                    <h4>Make Adsense</h4>
+                    <p>Add ads to your videos to earn money</p>
+                </div>
+                <button class="btn btn-main btn-block">Create now</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="main-content">
+        <header>
+            <div>
+                <label for="">
+                    <span class="las la-bars"></span>
+                </label>
+                <div>
+                    <h1>Analytics</h1>
+                    <p>Display anaytics about your Chanel <span class="las la-chart-line"></span></p>
+                </div>                
+            </div>
+            <div class="header-action">
+                <button class="btn btn-main">
+                    <span class="las la-video"></span>
+                    Upload
+                </button>
+            </div>
+        </header>
+
+        <main>
+
+        </main>
+    </div>
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
